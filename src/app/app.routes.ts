76e70220e8cbbd6a6f93b41d8a90dbddd665b4dc
mauguetta <mauriciolopezasencio@gmail.com>
2024-10-12 15:15:01 -0600

    dashboard y pantallas de bitacora y herramienta

---
+++ src/app/app.routes.ts
@@ -0,0 +1,40 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.component')
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./business/profile/profile.component')
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./business/tables/tables.component')
+            },
+            {
+                path: 'bitacora',
+                loadComponent: () => import('./business/bitacora/bitacora.component')
+            },
+            {
+                path: 'herramienta',
+                loadComponent: () => import('./business/herramienta/herramienta.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
---
+++ src/app/business/herramienta/herramienta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-herramienta',
+  standalone: true,
+  imports: [],
+  templateUrl: './herramienta.component.html',
+  styleUrl: './herramienta.component.css'
+})
+export default class HerramientaComponent {
+
+}
